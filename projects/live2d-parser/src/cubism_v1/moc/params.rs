917ccad2707eaf9c5975bbc934757d3f90a05241
Aster <galaster@foxmail.com>
2025-04-01 17:46:50 +0800

    Test string encoding byte by byte

--- projects/live2d-parser/src/cubism_v1/moc/params.rs
+++ projects/live2d-parser/src/cubism_v1/moc/params.rs
@@ -20,13 +20,20 @@ impl ParamList {
     /// The input data must be a valid moc file
     pub unsafe fn parse(data: &[u8], offset: usize) -> Result<Self, serde_json::Error> {
         let mut params = Vec::new();
-        let mut current_offset = offset;
+        let mut current_offset = 0x00000027;
 
         // Read parameter count using variable-length encoding
-        let out = u64::decode_var(&data);
+        let out = u32::decode_var(&data[0x00000019..]);
         println!("count: {:?}", out);
-        current_offset += 1;
-        let min_value = std::ptr::read(data.as_ptr().add(current_offset) as *const f32);
+        let out = i32::decode_var(&data[0x00000027..]);
+        println!("count: {:?}", out);
+        let out = i32::decode_var(&data[0x00000027..]);
+        println!("count: {:?}", out);
+        
+        
+        
+        // current_offset += 1;
+        let min_value = std::ptr::read(data.as_ptr().add(current_offset) as *const i32);
         let max_value = std::ptr::read(data.as_ptr().add(current_offset + 4) as *const f32);
         let default_value = std::ptr::read(data.as_ptr().add(current_offset + 8) as *const f32);
         let dd_value = std::ptr::read(data.as_ptr().add(current_offset + 12) as *const f32);
